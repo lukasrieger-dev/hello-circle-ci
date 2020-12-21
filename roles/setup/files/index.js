2e766dd2618178ac6b3991beea8b930a1c7865b9
lukasrieger-dev <rieger.lks@gmail.com>
2020-12-21 20:47:21 +0100

    final

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
-    res.end("Hello World!");
+    res.end("Hello World! By Cloud DevOps Master L.R.");
 });
 server.listen(3000);
